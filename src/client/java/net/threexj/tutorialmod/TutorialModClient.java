5ab40868c7e4f495e226daf555059eb6ac142da6
Noah <127223475+noahmrch@users.noreply.github.com>
2024-06-16 13:10:38 +0200

    Add files via upload

---
+++ src/client/java/net/threexj/tutorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package net.threexj.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
