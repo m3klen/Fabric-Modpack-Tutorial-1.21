df98b2f91425f0aab94821102705919c92e46570
Noah <127223475+noahmrch@users.noreply.github.com>
2024-06-16 16:58:15 +0200

    Add files via upload

---
+++ src/main/java/net/threexj/tutorialmod/datagen/ModLootTableProvider.java
@@ -0,0 +1,43 @@
+package net.threexj.tutorialmod.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricBlockLootTableProvider;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.Enchantments;
+import net.minecraft.item.Item;
+import net.minecraft.item.Items;
+import net.minecraft.loot.LootTable;
+import net.minecraft.loot.entry.ItemEntry;
+import net.minecraft.loot.entry.LeafEntry;
+import net.minecraft.loot.entry.LootPoolEntry;
+import net.minecraft.loot.function.ApplyBonusLootFunction;
+import net.minecraft.loot.function.SetCountLootFunction;
+import net.minecraft.loot.provider.number.UniformLootNumberProvider;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.RegistryWrapper;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModLootTableProvider extends FabricBlockLootTableProvider {
+    public ModLootTableProvider(FabricDataOutput dataOutput, CompletableFuture<RegistryWrapper.WrapperLookup> registryLookup) {
+        super(dataOutput, registryLookup);
+    }
+
+    @Override
+    public void generate() {
+        addDrop(Blocks.DIAMOND_BLOCK, OreDrops(Blocks.DIAMOND_BLOCK, Items.DIAMOND));
+    }
+
+    public LootTable.Builder OreDrops(Block drop, Item item) {
+        RegistryWrapper.Impl<Enchantment> impl = this.registryLookup.getWrapperOrThrow(RegistryKeys.ENCHANTMENT);
+        return this.dropsWithSilkTouch(drop,
+                (LootPoolEntry.Builder)this.applyExplosionDecay(drop, ((LeafEntry.Builder)
+                                        ItemEntry.builder(item)
+                                                .apply(SetCountLootFunction
+                                                        .builder(UniformLootNumberProvider.create(15.0f, 20.0f))))
+                                        .apply(ApplyBonusLootFunction
+                                                .oreDrops(impl.getOrThrow(Enchantments.FORTUNE)))));
+    }
+}
---
+++ src/main/java/net/threexj/tutorialmod/datagen/ModRecipeProvider.java
@@ -0,0 +1,30 @@
+package net.threexj.tutorialmod.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
+import net.minecraft.data.server.recipe.RecipeExporter;
+import net.minecraft.data.server.recipe.ShapedRecipeJsonBuilder;
+import net.minecraft.item.Items;
+import net.minecraft.recipe.book.RecipeCategory;
+import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.util.Identifier;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModRecipeProvider extends FabricRecipeProvider {
+    public ModRecipeProvider(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> registriesFuture) {
+        super(output, registriesFuture);
+    }
+
+    @Override
+    public void generate(RecipeExporter exporter) {
+        ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, Items.DIAMOND, 1)
+                .pattern("SSS")
+                .pattern("S#S")
+                .pattern("SSS")
+                .input('S', Items.STONE)
+                .input('#', Items.DIRT)
+                .criterion(hasItem(Items.STONE), conditionsFromItem(Items.STONE))
+                .offerTo(exporter, Identifier.tryParse(getRecipeName(Items.STONE)));
+    }
+}
